1b124b0ece8e702bf720e286e87d9e41b0956cbf
Horatiu Pacurar <horatiupacurar20@gmail.com>
2021-07-24 20:41:06 +0300

    second commit

--- src/main/java/ro/fasttrackit/curs21homework/movies/repository/ActorRepository.java
+++
@@ -1,7 +0,0 @@
-package ro.fasttrackit.curs21homework.movies.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ro.fasttrackit.curs21homework.movies.model.Actor;
-
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
-}
