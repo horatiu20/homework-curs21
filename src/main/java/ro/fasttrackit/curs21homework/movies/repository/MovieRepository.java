27654e2baed4d6a29f62554ce436bad0ad88c17a
Horatiu Pacurar <horatiupacurar20@gmail.com>
2021-07-25 09:30:51 +0300

    second commit

--- src/main/java/ro/fasttrackit/curs21homework/movies/repository/MovieRepository.java
+++ src/main/java/ro/fasttrackit/curs21homework/movies/repository/MovieRepository.java
@@ -3,7 +3,5 @@ package ro.fasttrackit.curs21homework.movies.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.fasttrackit.curs21homework.movies.model.Movie;
 
-import java.util.List;
-
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
